6d287c9e143048f8cd8e5ca0a0f25c9d0d52689d
rodriguez-jp <juanrdrz16@gmail.com>
2023-09-25 21:39:38 -0500

    Added the gray modal for scan

--- contentScript.js
+++ contentScript.js
@@ -1,1 +1,16 @@
-alert("Loaded");
+// const scanButton = document.getElementById("scan-button");
+
+// scanButton.addEventListener("click", beginScan);
+
+function beginScan() {
+  let grayLayout = document.getElementById("gray-layout");
+
+  if (!grayLayout) {
+    grayLayout = document.createElement("div");
+    grayLayout.classList.add("scan-modal");
+
+    document.body.appendChild(grayLayout);
+  }
+}
+
+beginScan();
