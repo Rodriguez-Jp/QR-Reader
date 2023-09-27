592908d5f5477435a8422f211341feb6d2defcac
rodriguez-jp <juanrdrz16@gmail.com>
2023-09-26 22:19:04 -0500

    Added the canvas display to draw the rectangle

--- contentScript.js
+++ contentScript.js
@@ -1,18 +1,90 @@
+let canvasOffset, offsetX, offsetY, startX, startY, ctx;
+let isDown = false;
+const bodyWidth = document.body.getAttribute;
+let pageSize = document.body.getBoundingClientRect();
+
 function beginScan() {
   let grayLayout = document.getElementById("gray-layout");
 
-  if (!grayLayout) {
-    grayLayout = document.createElement("div");
-    grayLayout.classList.add("scan-modal");
+  console.log(grayLayout.id);
 
-    document.body.appendChild(grayLayout);
+  if (grayLayout.id === undefined) {
+    console.log("Layout working");
+    return;
   }
 
-  document.body.addEventListener("mousedown", getPosition);
+  grayLayout = document.createElement("div");
+  grayLayout.classList.add("scan-modal");
+
+  let canvasElement = document.createElement("canvas");
+  canvasElement.classList.add("scan-rectangle");
+  canvasElement.id = "canvas";
+
+  grayLayout.appendChild(canvasElement);
+
+  document.body.appendChild(grayLayout);
+
+  let canvas = document.getElementById("canvas");
+  ctx = canvas.getContext("2d");
+
+  ctx.strokeStyle = "green";
+  ctx.lineWidth = 2;
+
+  canvasOffset = canvas.getBoundingClientRect();
+  canvas.width = pageSize.width;
+  canvas.height = pageSize.height;
+  offsetX = canvasOffset.left;
+  offsetY = canvasOffset.top;
+  isDown = false;
+
+  document
+    .getElementById("canvas")
+    .addEventListener("mousedown", (e) => mouseDown(e));
+  document
+    .getElementById("canvas")
+    .addEventListener("mouseup", (e) => mouseUp(e));
+  document
+    .getElementById("canvas")
+    .addEventListener("mousemove", (e) => mouseMove(e));
 }
 
-function getPosition(e) {
-  console.log(e);
+function mouseDown(e) {
+  e.preventDefault();
+  e.stopPropagation();
+
+  startX = parseInt(e.clientX - offsetX);
+  startY = parseInt(e.clientY - offsetY);
+  console.log(e.clientX, e.clientY);
+
+  isDown = true;
+}
+
+function mouseUp(e) {
+  e.preventDefault();
+  e.stopPropagation();
+
+  isDown = false;
+}
+
+function mouseMove(e) {
+  e.preventDefault();
+  e.stopPropagation();
+
+  if (!isDown) return;
+
+  let mouseX = parseInt(e.clientX - offsetX);
+  let mouseY = parseInt(e.clientY - offsetY);
+
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+
+  let width = mouseX - startX;
+  let height = mouseY - startY;
+
+  console.log(ctx);
+
+  console.table(mouseX, mouseY, startX, startY, width, height);
+
+  ctx.strokeRect(startX, startY, width, height);
 }
 
 chrome.runtime.onMessage.addListener(function (request) {
