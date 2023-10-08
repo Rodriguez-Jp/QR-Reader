df1b81304f8498ed2d4a61ca6653f6328a13dab2
rodriguez-jp <juanrdrz16@gmail.com>
2023-10-08 18:51:22 -0500

    Last commit before first refactor

--- src/background.js
+++ src/background.js
@@ -1,16 +1,3 @@
-// async function createOffscreen() {
-//   if (await chrome.offscreen.hasDocument()) {
-//     console.log("already created");
-//     return;
-//   }
-
-//   await chrome.offscreen.createDocument({
-//     url: "/offscreen.html",
-//     reasons: [chrome.offscreen.Reason.DOM_PARSER],
-//     justification: "testing",
-//   });
-// }
-
 chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
   if (request.action === "capture") {
     chrome.tabs.captureVisibleTab({ format: "png" }, (dataurl) => {
